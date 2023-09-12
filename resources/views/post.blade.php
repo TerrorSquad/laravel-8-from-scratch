956503c084da9027ecf3f05857c032a407b9a2ff
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-12 20:42:30 +0200

    init commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <?= $post ?>
+</article>
+<a href="./posts">Go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/post/my-first-post">My first post</a></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Quisquam, quibusdam. Quisquam, quibusdam.</p>
+</article>
+<article>
+    <h1><a href="/post/my-second-post">My second post</a></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Quisquam, quibusdam. Quisquam, quibusdam.</p>
+</article>
+<article>
+    <h1><a href="/post/my-third-post">My third post</a></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam. Quisquam, quibusdam. Quisquam, quibusdam.</p>
+</article>
+</body>
