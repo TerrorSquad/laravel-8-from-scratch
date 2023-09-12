84a9ca11c466c176cb8d4b31a9f7a97fcf5cdeb3
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-12 22:00:42 +0200

    lfs14: refactor blade templates to use blade features

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,23 @@
 <!doctype html>
 
+<?php
+/** @var \App\Models\Post[] $posts */
+
+?>
 <title>My Blog</title>
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<?php
-/** @var \App\Models\Post[] $posts */
-
-foreach ($posts as $post) : ?>
-    <article>
-        <h1>
-            <a href="/post/<?= $post->slug ?>">
-                <?= $post->title ?>
-            </a>
-        </h1>
-        <div>
-            <?= $post->excerpt ?>
-        </div>
-    </article>
-<?php endforeach; ?>
+@foreach ($posts as $post)
+<article class="{{ $loop->even ? 'foobar' : '' }}">
+    <h1>
+        <a href="/post/{{ $post->slug }}">
+            {{ $post->title}}
+        </a>
+    </h1>
+    <div>
+        {{ $post->excerpt }}
+    </div>
+</article>
+@endforeach
 </body>
