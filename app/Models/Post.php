849a328d5f2bb781179c03b945907edc4df9674e
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-13 00:43:50 +0200

    lfs21: remove post files, create Post migration and model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,53 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    /**
-     * @param $title
-     * @param $body
-     * @param $date
-     * @param $excerpt
-     * @param $slug
-     */
-    public function __construct(public $title, public $body, public $date, public $excerpt, public $slug) {}
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-        if (!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
-
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn($file) => YamlFrontMatter::parseFile((string) $file))
-                ->map(fn($document) => new Post(
-                    $document->title,
-                    $document->body(),
-                    $document->date,
-                    $document->excerpt,
-                    $document->slug
-                ))
-                ->sortByDesc('date');
-        });
-    }
-
+    use HasFactory;
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
     @foreach ($posts as $post)
     <article class="{{ $loop->even ? 'foobar' : '' }}">
         <h1>
-            <a href="/post/{{ $post->slug }}">
+            <a href="/post/{{ $post->id }}">
                 {{ $post->title}}
             </a>
         </h1>
