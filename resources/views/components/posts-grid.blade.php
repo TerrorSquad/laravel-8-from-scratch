9284268e63c2d5f2945083b467f959a591d9c3c7
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-16 21:18:01 +0200

    - Removed unnecessary comment in posts-grid.blade.php

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -6,7 +6,6 @@
 
 @if($posts->count() > 1)
     <div {{ $attributes->class(['lg:grid lg:grid-cols-6']) }}>
-        {{ /** @var \App\Models\Post[] $posts **/ }}
         @foreach($posts->skip(1) as $post)
             <x-post-card :post="$post" class="{{$loop->iteration < 3 ? 'col-span-3' : 'col-span-2'}}"/>
         @endforeach
