ce78764547b4c5c2954720e5c98a48b639f52690
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-17 03:01:22 +0200

    Refactor PostController index method to include query string in pagination
    The code changes in the PostController.php file refactor the index method. The 'posts' variable now includes a query string in pagination, allowing for better navigation and persistence of search, category, and author filters.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(\request(['search', 'category', 'author']))->paginate(6),
+            'posts' => Post::latest()->filter(\request(['search', 'category', 'author']))->paginate(6)->withQueryString(
+            ),
         ]);
     }
 
