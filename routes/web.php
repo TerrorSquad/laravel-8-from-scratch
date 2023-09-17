172a3bee99b90a51123a428751e8f23e57573dfb
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-17 02:09:45 +0200

    Refactor web routes, remove unused imports and route definition
    The commit refactors the web routes file by removing unused imports and a route definition that is no longer needed. This improves code cleanliness and removes unnecessary dependencies.

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('post/{post}', [PostController::class, 'show'])->name('post');
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts.index', [
-        'posts' => $author->posts
-    ]);
-})->name('author');
