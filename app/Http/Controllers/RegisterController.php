d63d22c4c81685decaf81f507611d30e98ca1cd6
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-17 16:22:51 +0200

    feat: Add RegisterController and registration form
    - Added a new file `RegisterController.php` in the `app/Http/Controllers` directory.
    - Implemented the `store()` method in the `RegisterController` to handle user registration.
    - Created a new view file `create.blade.php` in the `resources/views/register` directory for the registration form.
    - Updated the routes in `web.php` to include routes for registering a user.
    
    This commit adds functionality for user registration by introducing a new controller, implementing the registration logic, and creating a corresponding view for the registration form.

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => ['required', 'max:255'],
+            'username' => ['required', 'max:255', 'min:3'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'min:8', 'max:20']
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
+    }
+
+    public function create()
+    {
+        return view('register.create');
+    }
+}
