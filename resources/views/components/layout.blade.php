b3fd1a11b52d88df605cbe3be337b2144a0efd0b
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-18 18:57:53 +0200

    feat: Add Mailchimp SDK dependency and mailchimp configuration
    This commit adds the "mailchimp/marketing" package as a dependency in composer.json. It also includes the necessary changes in composer.lock to reflect the addition of the package. Additionally, it adds a new configuration entry for Mailchimp in services.php. Finally, it modifies layout.blade.php to include smooth scrolling behavior and an anchor link for newsletter subscription.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,11 @@
       rel="stylesheet">
 <script defer
         src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
 
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">
@@ -43,7 +48,7 @@
                    class="ml-4 text-xs font-bold uppercase">Log In</a>
             @endauth
 
-            <a href="#"
+            <a href="#newsletter"
                class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
@@ -52,7 +57,8 @@
 
     {{ $slot }}
 
-    <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+    <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
         <img src="/images/lary-newsletter-icon.svg"
              alt=""
              class="mx-auto -mb-6"
