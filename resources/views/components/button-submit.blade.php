37eb9a16d1806d202c6549ab0d6d25af71e6e400
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-09-18 18:31:29 +0200

    Add comment form component and include it in the post show view.
    - Added a new file `button-submit.blade.php` which contains the code for a submit button with specific styling.
    - Added a new file `_comment-form.blade.php` which contains the code for a comment form.
    - Modified the `show.blade.php` file to include the comment form component instead of having the form code directly in the file.

---
+++ resources/views/components/button-submit.blade.php
@@ -0,0 +1,4 @@
+<button type="submit"
+        {{ $attributes->class(['bg-blue-500 hover:bg-blue-600 text-white font-semibold text-xs py-2 px-10 rounded-2xl']) }}>
+    {{ $slot }}
+</button>
---
+++ resources/views/posts/_comment-form.blade.php
@@ -0,0 +1,49 @@
+@auth
+    <x-panel>
+        <form method="POST"
+              action="{{ route('post.comment', $post) }}">
+            @csrf
+            <header class="flex items-center">
+
+                <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}"
+                     alt=""
+                     width="40"
+                     height="40"
+                     class="rounded-full"
+                >
+                <h2 class="ml-4">Want to participate?</h2>
+            </header>
+
+
+            <div class="mt-6">
+                                <textarea name="body"
+                                          id="body"
+                                          rows="5"
+                                          class="text-sm w-full focus:outline-none focus:ring p-2"
+                                          placeholder="Quick think of something to say"
+                                          required></textarea>
+            </div>
+
+            @error('body')
+            <span class="text-xs text-red-500">{{ $message }}</span>
+            @enderror
+
+            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                <x-button-submit>Post</x-button-submit>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="">
+        <a href="{{ route('register.create') }}"
+           class="hover:underline font-semibold">
+            Register
+        </a>
+        or
+        <a href="{{ route('sessions.create') }}"
+           class="hover:underline font-semibold">
+            Login
+        </a>
+        to leave a comment.
+    </p>
+@endauth
